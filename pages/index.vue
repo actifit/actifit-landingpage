d13bf011a1bcf0485ed5c07fb6d322b60577a6ea
mktcode <kontakt@markus-kottlaender.de>
2018-07-16 13:56:47 +0200

    edited text

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <Header />
     <div class="card form mx-auto mt-4 p-3 text-center">
       <div class="form-group">
-        <p class="lead">Enter your Actifit username to see your current funds and transaction history.</p>
+        <p class="lead">Enter your Actifit/Steemit username to see your current funds and transaction history.</p>
         <input type="text" class="form-control" id="username" placeholder="Enter username" v-model="username" @keyup.enter="goToWallet()">
       </div>
       <a href="#" :class="'btn btn-danger' + (username.length < 3 ? ' disabled' : '')" @click="goToWallet()">Show Wallet</a>
