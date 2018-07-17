9e93510383cb523611766079a91603e43c53902c
mktcode <kontakt@markus-kottlaender.de>
2018-07-17 10:54:20 +0200

    added note and reward type to transaction display

--- components/Transaction.vue
+++ components/Transaction.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="card p-3 my-2 d-flex flex-row justify-content-between">
-    <div>
-      <h4 class="mb-0">{{ transaction.token_count }} <small>AFIT</small></h4>
-      <small class="text-muted">@{{ transaction.user }}</small>
+  <div class="card p-3 my-2">
+    <div class="d-flex flex-row justify-content-between">
+      <div>
+        <h4 class="mb-0">{{ transaction.token_count }} <small>AFIT</small></h4>
+        <small class="text-muted">Type: {{ transaction.reward_activity }}</small>
+      </div>
+      <div>
+        <small class="text-muted">{{ date }}</small><br>
+        <a :href="'https://steemit.com' + transaction.url" target="_blank" v-if="transaction.url">Activity Details</a>
+      </div>
     </div>
-    <div>
-      <small class="text-muted">{{ date }}</small><br>
-      <a :href="'https://steemit.com' + transaction.url" target="_blank">Activity Details</a>
-    </div>
+    <p class="font-italic mb-0 mt-2 text-center" v-if="transaction.note">{{ transaction.note }}</p>
   </div>
 </template>
 
