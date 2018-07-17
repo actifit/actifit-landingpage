86df7c59d47933b0913831430850e6c8e4a1fcd0
mktcode <kontakt@markus-kottlaender.de>
2018-07-17 13:32:58 +0200

    added comments, removed old component import

--- pages/wallet/_username.vue
+++ pages/wallet/_username.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container  pt-3 pb-5">
+    <!-- back to homepage -->
     <nuxt-link to="/"><i class="fas fa-arrow-left fa-2x text-brand"></i></nuxt-link>
+
+    <!-- header -->
     <div class="container py-5">
       <div class="text-center">
         <img src="/img/icon.png" alt="Actifit" />
@@ -8,12 +11,16 @@
         <h3 class="font-italic text-brand">Rewarding Fitness Activity</h3>
       </div>
     </div>
+
+    <!-- account balance -->
     <div class="text-center">
       <h3 class="mb-4">Hey {{ username }}!</h3>
       <h4>Your Account Balance:</h4>
       <h1 class="mb-0 font-weight-bold">{{ tokens }}</h1>
       <h5>Actifit Tokens</h5>
     </div>
+
+    <!-- transaction history -->
     <div class="history mx-auto">
       <h3 class="text-center mt-5">History</h3>
       <Transaction v-for="(transaction, index) in transactions" :key="index" :transaction="transaction" />
@@ -25,29 +32,32 @@
 <script>
   import 'whatwg-fetch' // fetch polyfill
 
-  import Header from '~/components/Header'
   import Transaction from '~/components/Transaction'
 
   export default {
     components: {
-      Header,
-      Transaction
+      Transaction // single transaction block
     },
     data () {
       return {
-        tokens: 0,
-        transactions: []
+        tokens: 0, // account balance
+        transactions: [] // transaction history
       }
     },
     computed: {
+      /**
+       * Returns username from route.
+       */
       username () {
         return this.$route.params.username
       }
     },
     mounted () {
+      // fetch account balance
       fetch('https://actifitbot.herokuapp.com/user/' + this.username).then(res => {
         res.json().then(json => this.tokens = json.tokens).catch(e => console.log(e.message))
       })
+      // fetch transaction history
       fetch('https://actifitbot.herokuapp.com/transactions/' + this.username).then(res => {
         res.json().then(json => this.transactions = json ? json : this.transactions).catch(e => console.log(e.message))
       })
