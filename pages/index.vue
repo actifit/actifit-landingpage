9673b410aa6a7c10f71705aecb0bbace8a0aee05
mktcode <kontakt@markus-kottlaender.de>
2018-08-07 17:28:32 +0200

    fixed signup link

--- pages/index.vue
+++ pages/index.vue
@@ -48,7 +48,7 @@
                 <a :href="$steemconnect.getLoginURL()" class="btn btn-brand btn-lg w-100">Login</a>
               </div>
               <div class="col-6">
-                <a href="https://steemit.com/signup" class="btn btn-brand btn-lg w-100">Sign Up</a>
+                <a href="https://signup.steemit.com/" class="btn btn-brand btn-lg w-100">Sign Up</a>
               </div>
             </div>
           </div>
