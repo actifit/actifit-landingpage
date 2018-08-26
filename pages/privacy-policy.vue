9de8be46abf98ac2c1f046f6489087807d4ed34f
Greateck <dev.wp@greateck.com>
2018-08-27 01:44:22 +0300

    minor fixes to privacy policy

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -73,7 +73,7 @@
 		</ul>
 
 		<h2>Transfer Of Data</h2>
-		<p>Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.</p>
+		<p>Your information, including Personal Data, may be transferred to - and maintained on - computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.</p>
 		<p>If you are located outside Lebanon and choose to provide information to us, please note that we transfer the data, including Personal Data, to Lebanon and process it there.</p>
 		<p>Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.</p>
 		<p>Actifit.io will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.</p>
@@ -127,8 +127,8 @@
 		<h2>Contact Us</h2>
 		<p>If you have any questions about this Privacy Policy, please contact us:</p>
 		<ul>
-			<li>By email: actifit.io@gmail.com</li>
-            <li>By visiting this page on our website: https://discord.gg/aHtcA6r</li>
+			<li>By email: <a href="mailto:actifit.io@gmail.com">actifit.io@gmail.com</a></li>
+            <li>By visiting our group on discord: https://discord.gg/aHtcA6r</li>
       
         </ul>
       
