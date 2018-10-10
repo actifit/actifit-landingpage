1b408bf7bad18a9578550f4f81c5ceb2a49aaa31
Greateck <dev.wp@greateck.com>
2018-10-10 16:38:42 +0300

    add fully functional image upload on edit report

--- components/EditReportModal.vue
+++ components/EditReportModal.vue
@@ -49,7 +49,7 @@
   const bucketName = 'actifit';
   
   AWS.config.update({ "accessKeyId": process.env.AWS_ACCESS_KEY_ID, "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY, "region":"us-east-1"});
-	console.log(AWS.config);
+
   export default {
     components: {
       InputTag
