04697e03a870613bfbe7e2bb5c3f4d8849c83840
Greateck <dev.wp@greateck.com>
2018-11-09 00:17:14 +0200

    add more adjustments and formatting to t&c

--- pages/terms-conditions.vue
+++ pages/terms-conditions.vue
@@ -21,20 +21,20 @@
 
 
 
-----
+<div>
 
-OVERVIEW
+<h4>OVERVIEW</h4>
 
-This website is operated by Actifit.io. Throughout the site, the terms “we”, “us” and “our” refer to Actifit.io. Actifit.io offers this website and related mobile apps, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.
+This website is operated by Actifit.io. Throughout the site, the terms "we", "us" and "our" refer to Actifit.io. Actifit.io offers this website and related mobile apps, including all information, tools and services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.
 
-By visiting our site and/ or using our apps, you engage in our “Service” and agree to be bound by the following terms and conditions (“Terms of Service”, “Terms”), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply  to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/ or contributors of content.
+By visiting our site and/ or using our apps, you engage in our "Service" and agree to be bound by the following terms and conditions ("Terms of Service", "Terms"), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply  to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/ or contributors of content.
 
 Please read these Terms of Service carefully before accessing or using our Service. By accessing or using any part of the site, you agree to be bound by these Terms of Service. If you do not agree to all the terms and conditions of this agreement, then you may not access the website or use any services. If these Terms of Service are considered an offer, acceptance is expressly limited to these Terms of Service.
 
 Any new features or tools which are added to the current Service shall also be subject to the Terms of Service. You can review the most current version of the Terms of Service at any time on this page. We reserve the right to update, change or replace any part of these Terms of Service by posting updates and/or changes to our website. It is your responsibility to check this page periodically for changes. Your continued use of or access to the website following the posting of any changes constitutes acceptance of those changes.
 
 
-SECTION 1 - ONLINE WEBSITE TERMS
+<h4>SECTION 1 - ONLINE ACTIFIT TERMS</h4>
 
 By agreeing to these Terms of Service, you represent that you are at least the age of majority in your state or province of residence, or that you are the age of majority in your state or province of residence and you have given us your consent to allow any of your minor dependents to use this site.
 
@@ -45,7 +45,7 @@ You must not transmit any worms or viruses or any code of a destructive nature.
 A breach or violation of any of the Terms will result in an immediate termination of your Services.
 
 
-SECTION 2 - GENERAL CONDITIONS
+<h4>SECTION 2 - GENERAL CONDITIONS</h4>
 
 We reserve the right to refuse service to anyone for any reason at any time.
 
@@ -56,14 +56,14 @@ You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion
 The headings used in this agreement are included for convenience only and will not limit or otherwise affect these Terms.
 
 
-SECTION 3 - ACCURACY, COMPLETENESS AND TIMELINESS OF INFORMATION
+<h4>SECTION 3 - ACCURACY, COMPLETENESS AND TIMELINESS OF INFORMATION</h4>
 
 We are not responsible if information made available on this site is not accurate, complete or current. The material on this site is provided for general information only and should not be relied upon or used as the sole basis for making decisions without consulting primary, more accurate, more complete or more timely sources of information. Any reliance on the material on this site is at your own risk.
 
 This site may contain certain historical information. Historical information, necessarily, is not current and is provided for your reference only. We reserve the right to modify the contents of this site at any time, but we have no obligation to update any information on our site. You agree that it is your responsibility to monitor changes to our site.
 
 
-SECTION 4 - MODIFICATIONS TO THE SERVICE AND PRICES
+<h4>SECTION 4 - MODIFICATIONS TO THE SERVICE AND PRICES</h4>
 
 Prices for our products are subject to change without notice.
 
@@ -72,7 +72,7 @@ We reserve the right at any time to modify or discontinue the Service (or any pa
 We shall not be liable to you or to any third-party for any modification, price change, suspension or discontinuance of the Service.
 
 
-SECTION 5 - PRODUCTS OR SERVICES (if applicable)
+<h4>SECTION 5 - PRODUCTS OR SERVICES (if applicable)</h4>
 
 Certain products or services may be available exclusively online through the website. These products or services may have limited quantities and are subject to return or exchange only according to our Return Policy.
 
@@ -81,25 +81,25 @@ We reserve the right, but are not obligated, to limit the sales of our products 
 We do not warrant that the quality of any products, services, information, or other material purchased or obtained by you will meet your expectations, or that any errors in the Service will be corrected.
 
 
-SECTION 6 - ACCURACY OF ACCOUNT INFORMATION
+<h4>SECTION 6 - ACCURACY OF ACCOUNT INFORMATION</h4>
 
 We reserve the right to refuse any order you place with us. We may, in our sole discretion, limit or cancel quantities purchased per person or per order. These restrictions may include orders placed by or under the same customer account. In the event that we make a change to or cancel an order, we may attempt to notify you by contacting the e-mail and/or billing address/phone number provided at the time the order was made.
 
 You acknowledge and understand that cryptography is a progressing field. Advances in code cracking or technical advances such as the development of quantum computers may present risks to the Services that you use and your Account, which could result in the theft or loss of your property. By using the Service or accessing Actifit Content, you acknowledge these inherent risks.
 
 
-SECTION 7 - OPTIONAL TOOLS
+<h4>SECTION 7 - OPTIONAL TOOLS</h4>
 
 We may provide you with access to third-party tools over which we neither monitor nor have any control nor input.
 
-You acknowledge and agree that we provide access to such tools ”as is” and “as available” without any warranties, representations or conditions of any kind and without any endorsement. We shall have no liability whatsoever arising from or relating to your use of optional third-party tools.
+You acknowledge and agree that we provide access to such tools "as is" and "as available" without any warranties, representations or conditions of any kind and without any endorsement. We shall have no liability whatsoever arising from or relating to your use of optional third-party tools.
 
 Any use by you of optional tools offered through the site is entirely at your own risk and discretion and you should ensure that you are familiar with and approve of the terms on which tools are provided by the relevant third-party provider(s).
 
 We may also, in the future, offer new services and/or features through the website (including, the release of new tools and resources). Such new features and/or services shall also be subject to these Terms of Service.
 
 
-SECTION 8 - THIRD-PARTY LINKS
+<h4>SECTION 8 - THIRD-PARTY LINKS</h4>
 
 Certain content, products and services available via our Service may include materials from third-parties.
 
@@ -110,21 +110,21 @@ We are not liable for any harm or damages of any sort made in connection with an
 
 
 
-SECTION 9 - USER COMMENTS, FEEDBACK AND OTHER SUBMISSIONS
+<h4>SECTION 9 - USER COMMENTS, FEEDBACK AND OTHER SUBMISSIONS</h4>
 
 If, at our request, you send certain specific submissions (for example contest entries) or without a request from us you send creative ideas, suggestions, proposals, plans, or other materials, whether online, by email, by postal mail, or otherwise (collectively, 'comments'), you agree that we may, at any time, without restriction, edit, copy, publish, distribute, translate and otherwise use in any medium any comments that you forward to us. We are and shall be under no obligation (1) to maintain any comments in confidence; (2) to pay compensation for any comments; or (3) to respond to any comments.
 
-We may, but have no obligation to, monitor, edit or remove content that we determine in our sole discretion are unlawful, offensive, threatening, libelous, defamatory, pornographic, obscene or otherwise objectionable or violates any party’s intellectual property or these Terms of Service.
+We may, but have no obligation to, monitor, edit or remove content that we determine in our sole discretion are unlawful, offensive, threatening, libelous, defamatory, pornographic, obscene or otherwise objectionable or violates any party's intellectual property or these Terms of Service.
 
 You agree that your comments will not violate any right of any third-party, including copyright, trademark, privacy, personality or other personal or proprietary right. You further agree that your comments will not contain libelous or otherwise unlawful, abusive or obscene material, or contain any computer virus or other malware that could in any way affect the operation of the Service or any related website. You may not use a false e-mail address, pretend to be someone other than yourself, or otherwise mislead us or third-parties as to the origin of any comments. You are solely responsible for any comments you make and their accuracy. We take no responsibility and assume no liability for any comments posted by you or any third-party.
 
 
-SECTION 10 - PERSONAL INFORMATION
+<h4>SECTION 10 - PERSONAL INFORMATION</h4>
 
 Your submission of personal information through our Service is governed by our Privacy Policy. To view our Privacy Policy.
 
 
-SECTION 11 - ERRORS, INACCURACIES AND OMISSIONS
+<h4>SECTION 11 - ERRORS, INACCURACIES AND OMISSIONS</h4>
 
 Occasionally there may be information on our site or in the Service that contains typographical errors, inaccuracies or omissions that may relate to product descriptions, pricing, promotions, offers, product shipping charges, transit times and availability. We reserve the right to correct any errors, inaccuracies or omissions, and to change or update information or cancel orders if any information in the Service or on any related website is inaccurate at any time without prior notice (including after you have submitted your order).
 
@@ -132,12 +132,12 @@ We undertake no obligation to update, amend or clarify information in the Servic
 
 
 
-SECTION 12 - PROHIBITED USES
+<h4>SECTION 12 - PROHIBITED USES</h4>
 
 In addition to other prohibitions as set forth in the Terms of Service, you are prohibited from using the site or its content: (a) for any unlawful purpose; (b) to solicit others to perform or participate in any unlawful acts; (c) to violate any international, federal, provincial or state regulations, rules, laws, or local ordinances; (d) to infringe upon or violate our intellectual property rights or the intellectual property rights of others; (e) to harass, abuse, insult, harm, defame, slander, disparage, intimidate, or discriminate based on gender, sexual orientation, religion, ethnicity, race, age, national origin, or disability; (f) to submit false or misleading information; (g) to upload or transmit viruses or any other type of malicious code that will or may be used in any way that will affect the functionality or operation of the Service or of any related website, other websites, or the Internet; (h) to collect or track the personal information of others; (i) to spam, phish, pharm, pretext, spider, crawl, or scrape; (j) for any obscene or immoral purpose; or (k) to interfere with or circumvent the security features of the Service or any related website, other websites, or the Internet. We reserve the right to terminate your use of the Service or any related website for violating any of the prohibited uses.
 
 
-SECTION 13 - DISCLAIMER OF WARRANTIES; ASSUMPTION OF RISK, LIMITATION OF LIABILITY
+<h4>SECTION 13 - DISCLAIMER OF WARRANTIES; ASSUMPTION OF RISK, LIMITATION OF LIABILITY</h4>
 
 We do not guarantee, represent or warrant that your use of our service will be uninterrupted, timely, secure or error-free.
 
@@ -145,7 +145,7 @@ We do not warrant that the results that may be obtained from the use of the serv
 
 You agree that from time to time we may remove the service for indefinite periods of time or cancel the service at any time, without notice to you.
 
-You accept and acknowledge that there are risks associated with utilizing an Internet-based Steem blockchain account service including, but not limited to, the risk of failure of hardware, software and Internet connections, the risk of malicious software introduction, and the risk that third-parties may obtain unauthorized access to information stored within or associated with your Account, including, but not limited to your private key(s) (“Private Key”). You accept and acknowledge that we will not be responsible for any communication failures, disruptions, errors, distortions, or delays you may experience when using the Services, however caused.
+You accept and acknowledge that there are risks associated with utilizing an Internet-based Steem blockchain account service including, but not limited to, the risk of failure of hardware, software and Internet connections, the risk of malicious software introduction, and the risk that third-parties may obtain unauthorized access to information stored within or associated with your Account, including, but not limited to your private key(s) ("Private Key"). You accept and acknowledge that we will not be responsible for any communication failures, disruptions, errors, distortions, or delays you may experience when using the Services, however caused.
 
 We make no representation or warranty of any kind, express or implied, statutory, or otherwise, regarding the contents of the Service, information and functions made accessible through the Service, any hyperlinks to third-party websites, nor for any breach of security associated with the transmission of information through the Service or any website linked to by the Service.
 
@@ -163,17 +163,17 @@ In no case shall Actifit.io, our directors, officers, employees, affiliates, age
 
 
 
-SECTION 14 - INDEMNIFICATION
+<h4>SECTION 14 - INDEMNIFICATION</h4>
 
-You agree to indemnify, defend and hold harmless Actifit.io and our parent, subsidiaries, affiliates, partners, officers, directors, agents, contractors, licensors, service providers, subcontractors, suppliers, interns and employees, harmless from any claim or demand, including reasonable attorneys’ fees, made by any third-party due to or arising out of your breach of these Terms of Service or the documents they incorporate by reference, or your violation of any law or the rights of a third-party.
+You agree to indemnify, defend and hold harmless Actifit.io and our parent, subsidiaries, affiliates, partners, officers, directors, agents, contractors, licensors, service providers, subcontractors, suppliers, interns and employees, harmless from any claim or demand, including reasonable attorneys' fees, made by any third-party due to or arising out of your breach of these Terms of Service or the documents they incorporate by reference, or your violation of any law or the rights of a third-party.
 
 
-SECTION 15 - SEVERABILITY
+<h4>SECTION 15 - SEVERABILITY</h4>
 
 In the event that any provision of these Terms of Service is determined to be unlawful, void or unenforceable, such provision shall nonetheless be enforceable to the fullest extent permitted by applicable law, and the unenforceable portion shall be deemed to be severed from these Terms of Service, such determination shall not affect the validity and enforceability of any other remaining provisions.
 
 
-SECTION 16 - TERMINATION
+<h4>SECTION 16 - TERMINATION</h4>
 
 The obligations and liabilities of the parties incurred prior to the termination date shall survive the termination of this agreement for all purposes.
 
@@ -182,7 +182,7 @@ These Terms of Service are effective unless and until terminated by either you o
 If in our sole judgment you fail, or we suspect that you have failed, to comply with any term or provision of these Terms of Service, we also may terminate this agreement at any time without notice and you will remain liable for all amounts due up to and including the date of termination; and/or accordingly may deny you access to our Services (or any part thereof).
 
 
-SECTION 17 - ENTIRE AGREEMENT
+<h4>SECTION 17 - ENTIRE AGREEMENT</h4>
 
 The failure of us to exercise or enforce any right or provision of these Terms of Service shall not constitute a waiver of such right or provision.
 
@@ -191,21 +191,23 @@ These Terms of Service and any policies or operating rules posted by us on this 
 Any ambiguities in the interpretation of these Terms of Service shall not be construed against the drafting party.
 
 
-SECTION 18 - GOVERNING LAW
+<h4>SECTION 18 - GOVERNING LAW</h4>
 
 These Terms of Service and any separate agreements whereby we provide you Services shall be governed by and construed in accordance with the laws of Lebanon.
 
 
-SECTION 19 - CHANGES TO TERMS OF SERVICE
+<h4>SECTION 19 - CHANGES TO TERMS OF SERVICE</h4>
 
 You can review the most current version of the Terms of Service at any time at this page.
 
 We reserve the right, at our sole discretion, to update, change or replace any part of these Terms of Service by posting updates and changes to our website. It is your responsibility to check our website periodically for changes. Your continued use of or access to our website or the Service following the posting of any changes to these Terms of Service constitutes acceptance of those changes.
 
 
-SECTION 20 - CONTACT INFORMATION
+<h4>SECTION 20 - CONTACT INFORMATION</h4>
 
 Questions about the Terms of Service should be sent to us at actifit.io@gmail.com.
+
+</div>
 
 </div>
 
