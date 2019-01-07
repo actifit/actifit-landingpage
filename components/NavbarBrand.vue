f36af4da8a27439ac6c56cece39e8833fde39f17
Greateck <dev.wp@greateck.com>
2019-01-07 19:51:53 +0200

    Change header display to conform with rest of site

--- components/NavbarBrand.vue
+++ components/NavbarBrand.vue
@@ -3,8 +3,8 @@
   <div class="mx-auto navbar-brand py-0">
     <img src="/img/icon.png" alt="Actifit" class="float-left mr-2" />
     <div class="float-left d-none d-sm-block">
-      <h1 class="m-0 text-brand">Actifit Fitness Tracker</h1>
-      <h3 class="m-0 font-italic text-brand">Rewarding Fitness Activity</h3>
+      <h1 class="m-0 text-brand">Actifit</h1>
+      <h3 class="m-0 font-italic text-brand">Rewarding Your Everyday Activity</h3>
     </div>
   </div>
 </template>
