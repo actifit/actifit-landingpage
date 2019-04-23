b356a9d35d5f14cc28ca8edb779223a846192e75
Greateck <dev.wp@greateck.com>
2019-04-24 01:31:04 +0300

    Adding capability to send out custom_json data via actifit.io / requesting permission

--- plugins/vue-steemconnect.js
+++ plugins/vue-steemconnect.js
@@ -4,5 +4,6 @@ import VueSteemConnect from 'vue-steemconnect'
 // init steemconnect (with default vote and comment permissions)
 Vue.use(VueSteemConnect, {
   app: 'actifit.app',
-  callbackURL: process.env.scRedirectUrl || 'http://localhost:3000/auth'
+  callbackURL: process.env.scRedirectUrl || 'http://localhost:3000/auth',
+  scope: ['vote', 'comment', 'custom_json']
 })
