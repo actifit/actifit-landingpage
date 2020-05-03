1a66eada5b5d8d8d4107745f7a0d82bdeecd6c47
Greateck <dev.wp@greateck.com>
2020-05-04 01:10:34 +0300

    Improve layout for stats display on profile

--- pages/_username/index.vue
+++ pages/_username/index.vue
@@ -36,45 +36,49 @@
 				<i class="fas fa-spin fa-spinner" v-if="friendshipLoader"></i>
 			</span>
 		</h5>
+		<h5 class="d-flex flex-row text-brand user-name" v-else-if="!noUserFound">
+			<a :href="formattedProfileUrl" target="_blank">@{{ displayUser }}</a>
+		</h5>
 		
 		<div class="d-flex flex-row">
         <div class="d-flex flex-column mb-3">
 		  <div v-if="displayUser" class="user-avatar large-avatar mr-1 mb-3"
 					   :style="'background-image: url(https://steemitimages.com/u/' + this.displayUser + '/avatar)'"></div>
-		  <div class="acti-widget">
-			<table v-if="1==2">
-				<tr>
-					<td colspan="4"><h6>Last Updated On </h6>
-						{{ pureDate(lastUpdated) }}
-					</td>
-				</tr>
+		  <div class="acti-widget" v-if="displayUser">
+			<table>
 				<tr>
 					<!--<img src="https://cdn.steemitimages.com/DQmdnh1nApZieHZ3s1fEhCALDjnzytFwo78zbAY5CLUMpoG/TRACKM.png">-->
 					<td><div class="text-center"><div class="phishy"><h6>Height</h6></div>
 						<h6>{{ lastHeight + ' '+heightUnit }}</h6></div></td>
-					<td><img src="https://cdn.steemitimages.com/DQmfSsFiXem7AxWG1NCiYYPAjtT4Y7LR8FsXpfsZQe7XqPC/h1.png"></td>
+					<td><img src="https://usermedia.actifit.io/height.png"></td>
 					<td><div class="text-center"><div class="phishy"><h6>Weight</h6></div>
 						<h6>{{ lastWeight+' '+weightUnit}}</h6></div></td>
-					<td><img src="https://cdn.steemitimages.com/DQmVqJVEWUwicFRtkEz2WYq2mDH61mQLDsrzN1yBrKLrpyZ/w1a.png"></td>
+					<td><img src="https://usermedia.actifit.io/weight.png"></td>
 				</tr>
 				
 				<tr>
 					<td><div class="text-center"><div class="phishy"><h6>Body Fat</h6></div>
 						<h6>{{ lastBodyfat+' % '}}</h6></div></td>
-					<td><img src="https://cdn.steemitimages.com/DQmPJ2Vvi3mBQXKHoy5CTG7fyLFWMG8JaAZ8y1XZFeDkRUC/bd1.png"></td>
+					<td><img src="https://usermedia.actifit.io/bodyfat.png"></td>
 					<td><div class="text-center"><div class="phishy"><h6>Chest</h6></div></div>
 						<div class="text-center"><h6>{{ lastChest+' '+chestUnit }}</h6></div></td>
-					<td><img src="https://cdn.steemitimages.com/DQmbaoNBT5Unnjqh8JgP6TPj4mFKFnyKkLgP6eDYnnkiLkB/c1.png"></td>
+					<td><img src="https://usermedia.actifit.io/chest.png"></td>
 				</tr>
 
 				<tr>
 					<td><div class="text-center"><div class="phishy"><h6>Waist</h6></div></div>
 						<div class="text-center"><h6>{{ lastWaist+' '+waistUnit }}</h6></div></td>
-					<td><img src="https://cdn.steemitimages.com/DQmZ2Lfwg77FLaf3YpU1VPLsJvnBt1F8DG8y6t6xUAKnsYq/w1.png"></td>
+					<td><img src="https://usermedia.actifit.io/waist.png"></td>
 					<td><div class="text-center"><div class="phishy"><h6>Thighs</h6></div></div>
 						<div class="text-center"><h6>{{ lastThighs+' '+thighsUnit }}</h6></div></td>
-					<td><img src="https://cdn.steemitimages.com/DQmbbAAFy6hwwBWqtSmcSwosTyNZi9rcd6GNeugQRY9MF1h/t1.png"></td>
+					<td><img src="https://usermedia.actifit.io/thighs.png"></td>
 					
+				</tr>
+				
+				<tr>
+					<td colspan="4" class="font-italic">
+						<small>Last Updated On {{ pureDate(lastUpdated) }}</small>
+					</td>
 				</tr>
 			</table>
 			
