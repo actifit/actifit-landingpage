bdffbf1678e6034b504c0a1a8770e12e5ada7988
Mohammad <mcfarhat@gmail.com>
2022-02-09 16:36:24 +0200

    deploy blurt integration announcement

--- components/CompetitionAnnounce.vue
+++ components/CompetitionAnnounce.vue
@@ -12,13 +12,13 @@
 			<!--<iframe v-if="screenWidth < 768" src="http://www.youtube.com/embed/kS6AXH1kY74" width="320" height="180" frameborder="0" class="mx-auto" allowfullscreen></iframe>
 			<iframe v-else src="https://www.youtube.com/embed/kS6AXH1kY74" width="640" height="360" frameborder="0" class="mx-auto" allowfullscreen></iframe>-->
 			<!--<iframe width="640" height="360" src="https://www.youtube.com/embed/kS6AXH1kY74"></iframe>-->
-			<img class="announce-img" src="/img/defi-soft-launch-announce.jpg">
+			<img class="announce-img" src="/img/blurt-actifit.jpg">
 		  </div>
 		  <div>
 		  <!--<a href="https://actifit.io/@actifit/we-have-winners-public-token-sale-kicks-off-today-t-7-hours" class="btn btn-brand m-2" target="_blank">Event Details</a>
 		  <a href="https://www.youtube.com/watch?v=Vc6rpDU99nk" class="btn btn-brand m-2" target="_blank">Video Announcement</a> 
 		  <a href="https://tokensale.actifit.io" class="btn btn-brand m-2 back-red" target="_blank">Join NOW!</a>-->
-		  <a href="https://defi.actifit.io" class="btn btn-brand m-2 back-red" target="_blank">Join NOW!</a>
+		  <a href="https://actifit.io/@actifit/hellooo-blurt" class="btn btn-brand m-2 back-red" target="_blank">Read More</a>
 		  </div>
 		  <!--<div><span class="end-string">Token Sale ends in </span><Countdown v-if="countDownReady" deadline="October 31, 2021 22:00 GMT"></Countdown><i v-else class="fas fa-spin fa-spinner text-brand"></i></div>-->
 		</div>
