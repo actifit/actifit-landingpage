f5da86fccc239c42f6777c7092be2bd10d272c86
Mohammad <mcfarhat@gmail.com>
2022-05-29 01:04:21 +0300

    expand amount of FAQ questions supported

--- pages/faq.vue
+++ pages/faq.vue
@@ -9,7 +9,7 @@
 			<h1 class="pt-5 mb-3 text-capitalize text-center headline"><span class="text-brand">{{ $t('faq.headline') }}</span></h1>
 			
 			
-			<div v-for="quest in 19" class="row lead mb-4 p-3 w-100" :key="quest">
+			<div v-for="quest in 100" class="row lead mb-4 p-3 w-100" :key="quest" v-if="$t('faq.q'+quest) != 'faq.q'+quest">
 				{{quest}}.&nbsp;<div v-html="$t('faq.q'+quest)" class="font-weight-bold"></div>
 				<div v-html="$t('faq.a'+quest)" class="w-100"></div>
 			</div>
