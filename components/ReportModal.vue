f50059cd5f3f8d8cf820b2071961b60b0f26c85c
Mohammad <mcfarhat@gmail.com>
2023-05-12 16:16:14 +0300

    adjust report title to h2

--- components/ReportModal.vue
+++ components/ReportModal.vue
@@ -9,7 +9,7 @@
 			</div>
 		</div>
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">{{ report.title }}</h5><br/>
+          <h2 class="modal-title" id="exampleModalLabel">{{ report.title }}</h2><br/>
 		  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
