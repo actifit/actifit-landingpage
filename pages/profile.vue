bf30d93192791a7773826cc1364599a5e05e0e0f
hasan-najjar11 <hassan.najjar11lb@gmail.com>
2024-06-14 10:33:46 +0300

    Changing the loading screen to match the app

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,65 +1,68 @@
 <template>
-
-    <!-- a text for the user whie it checks if logged in or not-->
-      <div class="loading-indicator">
-        Checking login status (^^)
+  <div>
+    <NavbarBrand /> 
+      <div class="loading-container">
+        <div class="text-center" v-if="loading">
+          <i class="fas fa-spinner fa-spin text-brand"></i>
+        </div>
       </div>
-  
-  
-    </template>
-    
-    <script>
-    import { mapGetters } from 'vuex'
-    
-    export default {
-      computed: {
-        ...mapGetters('steemconnect', ['user'])
-      },
-      methods: {
-        checkLoginStatus() {
-          if (process.client) {  // ensuring this code runs only on the client side so the check works
-            console.log('Checking login status:', this.user);
-            if (this.user && this.user.account) {
-              // if user is logged in, redirect to their /username (their username)
-              this.$router.replace(`/${this.user.account.name}`);
-            } else if(!this.user){
-              // if user is not logged in, redirect to /login
-              this.$router.replace('/login');
-            }
-          }
+    <Footer />
+  </div>
+</template>
+
+<script>
+import NavbarBrand from '~/components/NavbarBrand'
+import Footer from '~/components/Footer'
+import { mapGetters } from 'vuex'
+
+export default {
+  data() {
+    return {
+      loading: true
+    }
+  },
+  computed: {
+    ...mapGetters('steemconnect', ['user'])
+  },
+  methods: {
+    checkLoginStatus() {
+      if (process.client) {
+        if (this.user && this.user.account) {
+          this.$router.replace(`/${this.user.account.name}`);
+        } else if (!this.user) {
+          this.$router.replace('/login');
         }
-      },
-      watch: {
-        user(newUser) {
-          if (process.client) { 
-            console.log('User state changed:', newUser);
-            this.checkLoginStatus();
-          }
-        }
-      },
-      created() {
-        if (process.client) {  
-          this.$store.dispatch('steemconnect/login').then(() => {
-            console.log('Login action resolved');
-            this.checkLoginStatus();
-          }).catch(() => {
-            console.log('Login action rejected');
-            this.checkLoginStatus();
-          });
-        }
+        this.loading = false; // set loading to false after checking login status
       }
     }
-    </script>
-    
-    <style>
-    .loading-indicator {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      font-size: 1.5em;
-      font-weight: bold;
+  },
+  watch: {
+    user(newUser) {
+      if (process.client) {
+        this.checkLoginStatus();
+      }
     }
-    </style>
-  
-  
+  },
+  components: {
+	  NavbarBrand,
+	  Footer,
+  },
+  created() {
+    if (process.client) {
+      this.$store.dispatch('steemconnect/login').then(() => {
+        this.checkLoginStatus();
+      }).catch(() => {
+        this.checkLoginStatus();
+      });
+    }
+  }
+}
+</script>
+
+<style>
+.loading-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
